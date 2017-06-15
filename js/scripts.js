76b82ac9b011590419a9806d448ea82f2b57dcf2
Ondoro <ondoros4@gmail.com>
2017-06-15 20:15:38 +0300

    ,.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+//close
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+//add close/prevent default functionality to prevent submission of zero value answers
+//cofirm user
+$(document).ready(function() {
+  $("form#subsribe-users").submit(function(event) {
+    event.preventDefault();
+    var userEmail = $("input#user-email").val();
+    alert(userEmail + " " + "registration at Anita's Kitchen was a success thank you for helping us serve you better! Thank You.");
+
+  });
+});
